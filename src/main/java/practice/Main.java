daf65a6f1573561c3854239e9ffd6334fc2daf04
Vitaly Kshinin <vitaly.kshinin@gmail.com>
2022-11-10 16:03:09 +0300

    Homework 10.7 done

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,52 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        System.out.println("Введите команду LIST, ADD, EDIT или DELETE");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine().trim();
+            if (input.equals("0")) {
+                break;
+            }
+            int index = 0;
+
+            String regex = "(.+?)(\\s+(\\d+)?(.+)?)?";
+            String command = input.replaceAll(regex, "$1").trim();
+            String indexStr = input.replaceAll(regex, "$3").trim();
+            String todo = input.replaceAll(regex, "$4").trim();
+
+            if (!indexStr.isEmpty()) {
+                index = Integer.parseInt(indexStr);
+            }
+            switch (command) {
+                case "ADD": {
+                    if (index == 0) {
+                        todoList.add(todo);
+                        break;
+                    } else {
+                        todoList.add(index - 1, todo);
+                        break;
+                    }
+                }
+                case "EDIT":
+                    todoList.edit(index - 1, todo);
+                    break;
+                case "DELETE":
+                    todoList.delete(index - 1);
+                    break;
+                case "LIST":
+                    todoList.getTodos();
+                    break;
+                default:
+                    System.out.println("Введена неверная команда");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -4,28 +4,54 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    public  ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        todoList.add(todo);
+        System.out.println("Добавлено дело \"" + todo + "\"");
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (index < todoList.size() && index >= 0) {
+            todoList.add(index, todo);
+            System.out.println("Добавлено дело \"" + todo + "\"");
+        } else {
+            todoList.add(todo);
+            System.out.println("Добавлено дело \"" + todo + "\"");
+        }
     }
 
     public void edit(int index, String todo) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (index < todoList.size() && index >= 0) {
+            todoList.set(index, todo);
+            System.out.println("Дело \"" + todoList.get(index) + "\" заменено на \"" + todo + "\"");
+        } else {
+            System.out.println("Дело с таким номером не существует");
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index < todoList.size() && index >= 0) {
+            todoList.remove(index);
+            System.out.println("Дело \"" + todoList.get(index) + "\" удалено");
+        } else {
+            System.out.println("Дело с таким номером не существует");
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println(i + " - " + todoList.get(i).toString());
+        }
+        return todoList;
     }
 
 }
